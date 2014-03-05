0a483e2d6839c18747758ab51fc336b17f125f59
Tavoarsa <tavoarsa@gmail.com>
2014-03-05 11:58:59 -0600

    UdpateRecommend

--- CurriculumApp/CurriculumApp/Controllers/HomeController.cs
+++ CurriculumApp/CurriculumApp/Controllers/HomeController.cs
@@ -5,15 +5,18 @@ using System.Net.Mail;
 using System.Web;
 using System.Web.Mvc;
 using Postal;
+using CurriculumApp.Models;
 
 namespace CurriculumApp.Controllers
 {
     public class HomeController : Controller
     {
-        //
-        // GET: /Home/
 
-        public ActionResult Index()
+        private RecommendDb db = new RecommendDb();
+         //
+        // GET: /Home/
+        
+        public ActionResult Home()
         {
             /*dynamic email = new Email("Example");
             email.To = "webninja@example.com";
@@ -29,8 +32,64 @@ namespace CurriculumApp.Controllers
 
         public ActionResult Recommend()
         {
+
+            List<Recommend> recommends = db.Recommeds.ToList();
+            ViewBag.recommends = recommends;
+            return View();
+            
+        }
+
+
+        public ActionResult Create()
+        {
             return View();
         }
+    
+    [HttpPost]
+        public ActionResult Create(string nombre="",int telefono=0,string email="",string comentario="")
+        {
+        Recommend nueva = new Recommend();
+            nueva.Nombre = nombre;
+            nueva.Telefono = telefono;
+            nueva.Email = email;
+            nueva.Comentario = comentario;
+            if (ModelState.IsValid)
+            {
+                db.Recommeds.Add(nueva);
+                db.SaveChanges();
+                return RedirectToAction("Recommend");
+            }
+            return View();
+
+
+        }
+
+    public ActionResult Delete(int id)
+    {
+        Recommend recomend = db.Recommeds.Find(id);
+        if (recomend == null)
+        {
+            return HttpNotFound();
+
+        }
+        ViewBag.recomend = recomend;
+        return View();
+    }
+
+    [HttpPost, ActionName("Delete")]
+    public ActionResult DeleteConfirmed(int id)
+    {
+        Recommend recomend = db.Recommeds.Find(id);
+        db.Recommeds.Remove(recomend);
+        db.SaveChanges();
+        return RedirectToAction("Recommend");
+    }
+
+    protected override void Dispose(bool disposing)
+    {
+        db.Dispose();
+        base.Dispose(disposing);
+    }
 
                
 
