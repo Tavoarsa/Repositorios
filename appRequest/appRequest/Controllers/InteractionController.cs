23d5db9b5fdc8c24a3f274b148a4f541204a1499
Tavoarsa <tavoarsa@gmail.com>
2014-04-01 22:46:50 -0600

    Create Controllers

---
+++ appRequest/appRequest/Controllers/InteractionController.cs
@@ -0,0 +1,71 @@
+﻿using appRequest.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace appRequest.Controllers
+{
+    public class InteractionController : Controller
+    {
+
+        private ContextDb db = new ContextDb();
+        //
+        // GET: /Interaction/
+
+        public ActionResult Index()
+        {
+            List<Interaction> interaction = db.Interactions.ToList();
+            ViewBag.interactions = interaction;        
+            return View();
+        }
+        public ActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Create(Parameter parameter)
+        {
+            Interaction newInteraction = new Interaction();
+            newInteraction.Parameter = parameter;
+            if (ModelState.IsValid)
+            {
+                db.Interactions.Add(newInteraction);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View();
+        }
+        public ActionResult Edit()
+        {
+            return View();
+
+        }
+        [HttpPost]
+        public ActionResult Edit()
+        {
+            return View();
+        }
+        public ActionResult Delete()
+        {
+            return View();
+        }
+
+        [HttpPost, ActionName("Delete")]
+        public ActionResult DeleteConfirmed()
+        {
+            return View();
+
+        }
+
+
+        protected override void Dispose(bool disposing)
+        {
+            db.Dispose();
+            base.Dispose(disposing);
+        }
+
+    }
+}
---
+++ appRequest/appRequest/Models/ContextDb.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace appRequest.Models
+{
+    public class ContextDb:DbContext 
+    {
+        public ContextDb()
+            : base("DefaultConnection")
+        {
+
+        }
+        public DbSet<Interaction> Interactions {get; set;}
+        public DbSet<Parameter> Parameters {get; set;}
+        public DbSet<Request> Requests {get; set;}
+        public DbSet<Results> Results {get; set;}
+    }
+}
